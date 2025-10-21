d37cbe568225e75918d42166e3349371b86b6a5d
rdralves <rdr.alves@gmail.com>
2025-10-21 09:29:38 -0300

    Criado o endpoint para teste da pagina home

--- app/routes/auth_user.py
+++ app/routes/auth_user.py
@@ -1,3 +1,8 @@
 from flask import Blueprint, request, jsonify
 
 bp_user = Blueprint('bp_user', __name__)
+
+@bp_user.route('/')
+def home():
+    return "Bem vindo à página de autenticação/cadastro dos usuários!"
+
