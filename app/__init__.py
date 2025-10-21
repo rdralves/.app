bb99a580ac9c749b9b93f1411ab5ebf0f45f24ba
rdralves <rdr.alves@gmail.com>
2025-10-21 12:15:22 -0300

    feat: Criado as tabelas do banco em app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, app
+from .models.model import db
+
 
 def create_app():
 	app = Flask(__name__)
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+	db.init_app(app)
 
+    # Register blueprints/endpoints
 	from .routes.auth_user import bp_user
 	app.register_blueprint(bp_user)
+
+	# Criaçao do banco de dados
+    
+	with app.app_context():
+		db.create_all()
 	return app
+
+
+
