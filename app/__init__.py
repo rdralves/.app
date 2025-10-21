f09e801b6d4a4f68f82f7099413e6d16ee9948d8
rdralves <rdr.alves@gmail.com>
2025-10-21 09:34:14 -0300

    Concertando a importação relativa do endpoint routes/auth_user

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from flask import Flask
 def create_app():
 	app = Flask(__name__)
 
-	from routes.auth_user import bp_user
-	app.register_blueprint(bp_user, url_prefix='/auth')
+	from .routes.auth_user import bp_user
+	app.register_blueprint(bp_user)
 	return app
