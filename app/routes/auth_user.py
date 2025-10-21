1dd06e027eee029b45e4c0a1088e97496c5ea030
rdralves <rdr.alves@gmail.com>
2025-10-21 12:30:44 -0300

    feat endpoint_cadastro: Criado endpoint /register com metodo post, para receber os dados e salvar no banco de dados

--- app/routes/auth_user.py
+++ app/routes/auth_user.py
@@ -1,8 +1,22 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, render_template, request, jsonify
+from app.models.model import User, db
 
 bp_user = Blueprint('bp_user', __name__)
+
 
 @bp_user.route('/')
 def home():
     return "Bem vindo à página de autenticação/cadastro dos usuários!"
 
+
+@bp_user.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        username = request.get('username')
+        password = request.get('password')
+        email = request.get('email')
+
+        user = User.create_user(name=username, password=password, email=email)
+        db.session.add(user)
+        db.session.commit()
+        return render_template('register.html')
