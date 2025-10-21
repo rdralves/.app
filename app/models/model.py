ed99ad6cb1cd710f35855cc1c2ba488eb31b3f30
rdralves <rdr.alves@gmail.com>
2025-10-21 12:01:27 -0300

    feat: Criado modelo de dados em app/models/model.py

---
+++ app/models/model.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    name = db.Column(db.String(150), nullable=False)
+    
+    
+    def __repr__(self):
+        return f'<User {self.email}, {self.name}>'
+    
+    
