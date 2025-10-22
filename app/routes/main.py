4344ca3011a685fd5571d205178ad29de48e7f7a
rdralves <rdr.alves@gmail.com>
2025-10-22 13:39:17 -0300

    feature styles: Adicionado extends ao template login e register.html

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import current_user
 
 
 bp_main = Blueprint('bp_main', __name__)
@@ -6,4 +7,4 @@ bp_main = Blueprint('bp_main', __name__)
 
 @bp_main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', user=current_user)
