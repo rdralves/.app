9448869ffe8b25701a705ac976c866e9c49e231b
rdralves <rdr.alves@gmail.com>
2025-10-28 13:30:30 -0300

    Feature Veiculos: registro de blueprints no arquivo app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -25,8 +25,10 @@ def create_app():
     # Register blueprints/endpoints
 	from app.routes.main import bp_main
 	from app.routes.auth_user import bp_user
+	from app.routes.veiculos_routes import veiculos_bp
 	app.register_blueprint(bp_main)
 	app.register_blueprint(bp_user)
+	app.register_blueprint(veiculos_bp)
 
 	# Criaçao do banco de dados    
 	with app.app_context():
