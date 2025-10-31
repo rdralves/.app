bbee0a76ab3b0b19625fd3e10d8bd640ca864e59
rdralves <rdr.alves@gmail.com>
2025-10-31 13:19:01 -0300

    feature/motorista: Criado o modelo de banco de dados uso_veiculo.py em app/models/ para ter o historico de utilização

---
+++ app/models/uso_veiculo.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from app import db
+
+
+class UsoVeiculo(db.Model):
+    __tablename__ = 'usos_veiculo'
+
+    id = db.Column(db.Integer, primary_key=True)
+    motorista_id = db.Column(db.Integer, db.ForeignKey(
+        'motoristas.id'), nullable=False)
+    veiculo_id = db.Column(db.Integer, db.ForeignKey(
+        'veiculo.id'), nullable=False)
+    data_inicio = db.Column(db.DateTime, default=datetime.utcnow)
+    data_fim = db.Column(db.DateTime)
+    observacoes = db.Column(db.String(200))
+
+    veiculo = db.relationship('Veiculo', backref='usos', lazy=True)
+
+    def __repr__(self):
+        return f'<UsoVeiculo {self.motorista_id} - {self.veiculo_id}>'
